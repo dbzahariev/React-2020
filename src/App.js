5a9839ec48f18045d6ecfe8b93e428629d7c508e
Dimitar Zahariev <dbzahariev@gmail.com>
2020-12-15 11:42:56 +0200

    Fix App.js

--- src/App.js
+++ src/App.js
@@ -4,23 +4,9 @@ import WeatherEngine from "./components/WeatherEngine";
 
 function App() {
   return (
-    <div
-      // className="App"
-      style={{
-        alignContent: "center",
-        alignItems: "center",
-        display: "flex",
-        flexDirection: "row",
-      }}
-    >
-      {/* <div
-        style={{ backgroundColor: "red", width: "100px", height: "200px" }}
-      ></div>
-      <div
-        style={{ backgroundColor: "green", width: "100px", height: "200px" }}
-      ></div> */}
-      {/* {/* <WeatherEngine location={"Plovdiv"} /> */}
-      <WeatherEngine />
+    <div className="App">
+      <WeatherEngine location={"Plovdiv"} />
+      <WeatherEngine location={"Varna"} />
     </div>
   );
 }
